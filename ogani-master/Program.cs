2f916aa48a1357c2194be0a3dd4f18fb7c46705e
HiepVo <hoclaptrinh1012005@gmail.com>
2024-12-14 10:12:52 +0700

    Update project to .NET 8.0 and compatible package versions

--- ogani-master/Program.cs
+++ ogani-master/Program.cs
@@ -26,11 +26,10 @@ namespace ogani_master
 
             app.UseAuthorization();
 
-            app.MapStaticAssets();
+            app.UseStaticFiles();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-                .WithStaticAssets();
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
