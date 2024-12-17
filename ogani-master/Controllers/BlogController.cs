a8fb911935f2561d6630a75b2b63313b7e03f690
HiepVo <hoclaptrinh1012005@gmail.com>
2024-12-17 14:05:15 +0700

    Add static Blog page layout

---
+++ ogani-master/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ogani_master.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
