b9b89f947d3a414445c5c8ea957bbf916e45c892
HiepVo <hoclaptrinh1012005@gmail.com>
2024-12-17 14:15:48 +0700

    Add static Checkout Page Layout

---
+++ ogani-master/Controllers/CheckoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ogani_master.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
